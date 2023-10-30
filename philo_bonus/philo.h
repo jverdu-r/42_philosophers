621f0f06f9722b28343d703b403bff78d702fcb7
Jorge Verdu ruiz <jverdu-r@c2r6s1.42madrid.com>
2023-10-30 15:56:00 +0100

    DATA RACES FINNALY ELIMINATED

--- philo_bonus/philo.h
+++ philo_bonus/philo.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jverdu-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 15:52:59 by jverdu-r          #+#    #+#             */
+/*   Updated: 2023/10/30 15:54:44 by jverdu-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
@@ -12,21 +23,21 @@
 
 typedef struct s_philo
 {
-	int		id;
-	int		eat_cnt;
-	long long	last_eat_time;
-	pid_t		pid;
+	int				id;
+	int				eat_cnt;
+	long long		last_eat_time;
+	pid_t			pid;
 	struct s_info	*info;
 }	t_philo;
 
 typedef struct s_info
 {
-	int			num_philo;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			num_must_eat;
-	int			finish;
+	int				num_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_must_eat;
+	int				finish;
 	long long		start_time;
 	t_philo			*philos;
 	sem_t			*forks;
@@ -34,13 +45,13 @@ typedef struct s_info
 	sem_t			*eat_sem;
 }					t_info;
 
-int		print_error(char *message);
-int		ft_atoi(const char *nptr);
+int			print_error(char *message);
+int			ft_atoi(const char *nptr);
 long long	get_time(void);
 void		print_status(t_philo *philo, const char *message);
 void		ft_sleep(long long ms);
-int		init_info(t_info *info, int ac, char **av);
-int		init_philos(t_info *info);
+int			init_info(t_info *info, int ac, char **av);
+int			init_philos(t_info *info);
 void		fork_philos(t_info *info);
 void		*check_dead(void *arg);
 void		philo_start(t_philo *philo);
